7487acc569939c01b16bf6ed742048b0bdebfcc1
Valentin CHEVREAU <vchevreau37@gmail.com>
2020-01-09 19:16:12 +0100

    update: use of widgets to build calculator design

--- lib/page/flutter_exercise_1b.dart
+++ lib/page/flutter_exercise_1b.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_flutter/widget/calculator_button_widget.dart';
 
 class FlutterExcercise1b extends StatefulWidget {
   @override
@@ -9,311 +10,162 @@ class _FlutterExcercise1bState extends State<FlutterExcercise1b> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              child: Container(
-                child: Align(
+        backgroundColor: Colors.black,
+        body: Padding(
+          padding: const EdgeInsets.only(left: 18.0),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                    child: Align(
                   alignment: Alignment.bottomRight,
                   child: Row(
                     children: <Widget>[
-                      Text(
-                        '0',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 100.0,
-                        ),
-                      ),
+                      Text('0',
+                          style:
+                              TextStyle(color: Colors.white, fontSize: 100.0)),
                     ],
                   ),
+                )),
+              ),
+              Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Expanded(
+                          flex: 1,
+                          child: CalculatorButtonWidget('AC', Colors.grey)),
+                      Expanded(
+                          flex: 1,
+                          child: CalculatorButtonWidget('±', Colors.grey)),
+                      Expanded(
+                          flex: 1,
+                          child: CalculatorButtonWidget('%', Colors.grey)),
+                      Expanded(
+                          flex: 1,
+                          child: CalculatorButtonWidget('÷', Colors.orange)),
+                    ],
+                  )),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: CalculatorButtonWidget('7', Colors.white12),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: CalculatorButtonWidget('8', Colors.white12),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: CalculatorButtonWidget('9', Colors.white12),
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: CalculatorButtonWidget('×', Colors.orange),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('AC');
-                    },
-                    child: Text(
-                      "AC",
-                      style: TextStyle(color: Colors.black, fontSize: 35.0),
-                    ),
-                    fillColor: Colors.grey,
-                  ),
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('±');
-                    },
-                    child: Text(
-                      "±",
-                      style: TextStyle(color: Colors.black, fontSize: 35.0),
-                    ),
-                    fillColor: Colors.grey,
-                  ),
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('%');
-                    },
-                    child: Text(
-                      "%",
-                      style: TextStyle(color: Colors.black, fontSize: 35.0),
-                    ),
-                    fillColor: Colors.grey,
-                  ),
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('÷');
-                    },
-                    child: Text(
-                      "÷",
-                      style: TextStyle(color: Colors.white, fontSize: 35.0),
-                    ),
-                    fillColor: Colors.orange,
-                  ),
-                ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Expanded(
+                        flex: 1,
+                        child: CalculatorButtonWidget(
+                          '4',
+                          Colors.white12,
+                        )),
+                    Expanded(
+                        flex: 1,
+                        child: CalculatorButtonWidget(
+                          '5',
+                          Colors.white12,
+                        )),
+                    Expanded(
+                        flex: 1,
+                        child: CalculatorButtonWidget(
+                          '6',
+                          Colors.white10,
+                        )),
+                    Expanded(
+                        flex: 1,
+                        child: CalculatorButtonWidget(
+                          '–',
+                          Colors.orange,
+                        )),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('7');
-                    },
-                    child: Text(
-                      "7",
-                      style: TextStyle(color: Colors.white, fontSize: 35.0),
-                    ),
-                    fillColor: Color.fromRGBO(56, 54, 56, 1.0),
-                  ),
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('8');
-                    },
-                    child: Text(
-                      "8",
-                      style: TextStyle(color: Colors.white, fontSize: 35.0),
-                    ),
-                    fillColor: Color.fromRGBO(56, 54, 56, 1.0),
-                  ),
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('9');
-                    },
-                    child: Text(
-                      "9",
-                      style: TextStyle(color: Colors.white, fontSize: 35.0),
-                    ),
-                    fillColor: Color.fromRGBO(56, 54, 56, 1.0),
-                  ),
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('×');
-                    },
-                    child: Text(
-                      "×",
-                      style: TextStyle(color: Colors.white, fontSize: 35.0),
-                    ),
-                    fillColor: Colors.orange,
-                  ),
-                ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Expanded(
+                        flex: 1,
+                        child: CalculatorButtonWidget(
+                          '1',
+                          Colors.white12,
+                        )),
+                    Expanded(
+                        flex: 1,
+                        child: CalculatorButtonWidget(
+                          '2',
+                          Colors.white12,
+                        )),
+                    Expanded(
+                        flex: 1,
+                        child: CalculatorButtonWidget(
+                          '3',
+                          Colors.white12,
+                        )),
+                    Expanded(
+                        flex: 1,
+                        child: CalculatorButtonWidget(
+                          '+',
+                          Colors.orange,
+                        )),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('4');
-                    },
-                    child: Text(
-                      "4",
-                      style: TextStyle(color: Colors.white, fontSize: 35.0),
-                    ),
-                    fillColor: Color.fromRGBO(56, 54, 56, 1.0),
-                  ),
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('5');
-                    },
-                    child: Text(
-                      "5",
-                      style: TextStyle(color: Colors.white, fontSize: 35.0),
-                    ),
-                    fillColor: Color.fromRGBO(56, 54, 56, 1.0),
-                  ),
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('6');
-                    },
-                    child: Text(
-                      "6",
-                      style: TextStyle(color: Colors.white, fontSize: 35.0),
-                    ),
-                    fillColor: Color.fromRGBO(56, 54, 56, 1.0),
-                  ),
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('–');
-                    },
-                    child: Text(
-                      "–",
-                      style: TextStyle(color: Colors.white, fontSize: 35.0),
-                    ),
-                    fillColor: Colors.orange,
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('1');
-                    },
-                    child: Text(
-                      "1",
-                      style: TextStyle(color: Colors.white, fontSize: 35.0),
-                    ),
-                    fillColor: Color.fromRGBO(56, 54, 56, 1.0),
-                  ),
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('2');
-                    },
-                    child: Text(
-                      "2",
-                      style: TextStyle(color: Colors.white, fontSize: 35.0),
-                    ),
-                    fillColor: Color.fromRGBO(56, 54, 56, 1.0),
-                  ),
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('3');
-                    },
-                    child: Text(
-                      "3",
-                      style: TextStyle(color: Colors.white, fontSize: 35.0),
-                    ),
-                    fillColor: Color.fromRGBO(56, 54, 56, 1.0),
-                  ),
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('+');
-                    },
-                    child: Text(
-                      "+",
-                      style: TextStyle(color: Colors.white, fontSize: 35.0),
-                    ),
-                    fillColor: Colors.orange,
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    height: 65.0,
-                    width: MediaQuery.of(context).size.width / 2.4,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30.0),
-                        color: Color.fromRGBO(56, 54, 56, 1.0)),
-                    child: MaterialButton(
-                      onPressed: () {
-                        _buttonAction('0');
-                      },
-                      child: Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          "0",
-                          style: TextStyle(color: Colors.white, fontSize: 35.0),
-                        ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 4,
+                      child: CalculatorButtonWidget(
+                        '0',
+                        Colors.white12,
                       ),
                     ),
-                  ),
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction(',');
-                    },
-                    child: Text(
-                      ",",
-                      style: TextStyle(color: Colors.white, fontSize: 35.0),
+                    Expanded(
+                      flex: 2,
+                      child: CalculatorButtonWidget(
+                        ',',
+                        Colors.white12,
+                      ),
                     ),
-                    fillColor: Color.fromRGBO(56, 54, 56, 1.0),
-                  ),
-                  RawMaterialButton(
-                    shape: const CircleBorder(),
-                    constraints: BoxConstraints.tight(Size(70.0, 70.0)),
-                    onPressed: () {
-                      _buttonAction('=');
-                    },
-                    child: Text(
-                      "=",
-                      style: TextStyle(color: Colors.white, fontSize: 35.0),
-                    ),
-                    fillColor: Colors.orange,
-                  ),
-                ],
+                    Expanded(
+                      flex: 2,
+                      child: CalculatorButtonWidget(
+                        '=',
+                        Colors.orange,
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  _buttonAction(text) {
-    print(text);
+            ],
+          ),
+        ));
   }
 }
---
+++ lib/widget/calculator_button_widget.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+class CalculatorButtonWidget extends StatefulWidget {
+  final String title;
+  final Color color;
+  //final Function onPressed;
+
+  CalculatorButtonWidget(this.title, this.color); //this.onPressed);
+
+  @override
+  _CalculatorButtonWidgetState createState() => _CalculatorButtonWidgetState();
+}
+
+class _CalculatorButtonWidgetState extends State<CalculatorButtonWidget> {
+  Color _color;
+
+  Size _size;
+
+  @override
+  void initState() {
+    super.initState();
+    _color = Colors.blue;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (detail) {},
+      onTapCancel: () {
+        setState(() {
+          _color = widget.color;
+        });
+      },
+      onTapUp: (detail) {},
+      child: Row(
+        children: <Widget>[
+          RawMaterialButton(
+            shape: const CircleBorder(),
+            constraints: BoxConstraints.tight(Size(70.0, 70.0)),
+            onPressed: () {},
+            child: Text(
+              widget.title,
+              style: TextStyle(color: Colors.white, fontSize: 35.0),
+            ),
+            fillColor: widget.color,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/*
+class CalculatorButtonWidgetSpecific extends StatefulWidget {
+  final String title;
+  final Color color;
+
+  CalculatorButtonWidgetSpecific(this.title, this.color);
+
+  @override
+  _CalculatorButtonWidgetSpecificState createState() =>
+      _CalculatorButtonWidgetSpecificState();
+}
+
+class _CalculatorButtonWidgetSpecificState
+    extends State<CalculatorButtonWidgetSpecific> {
+  Color _color;
+
+  Size _size;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: (detail) {
+        setState(() {
+          _color = widget.color;
+        });
+      },
+      onTapCancel: () {
+        setState(() {
+          _color = Colors.blue;
+        });
+      },
+      onTapUp: (detail) {
+        // widget.buttonPressed();
+        setState(() {
+          _color = Colors.blue;
+        });
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            height: 65.0,
+            width: MediaQuery.of(context).size.width / 2.4,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0), color: widget.color),
+            child: MaterialButton(
+              onPressed: () {},
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  widget.title,
+                  style: TextStyle(color: Colors.white, fontSize: 35.0),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}*/
