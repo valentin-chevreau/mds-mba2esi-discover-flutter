904b1589bb5417971472828617f0a5c4438faaeb
Valentin CHEVREAU <vchevreau37@gmail.com>
2020-01-09 11:47:50 +0100

    update: login page example - Use widgets to optimize code

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -18,71 +18,86 @@ class _LoginPageState extends State<LoginPage> {
         title: Text('Login'),
         centerTitle: true,
       ),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    Container(height: 32.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          Icons.account_circle,
-                          color: Colors.blue.withOpacity(0.6),
-                          size: 64.0,
-                        ),
-                      ],
-                    ),
-                    Container(height: 16.0),
-                    TextField(
-                      decoration: InputDecoration(
-                        isDense: true,
-                        labelText: 'Username',
-                        focusedBorder: _textFieldBorder(isFocused: true),
-                        enabledBorder: _textFieldBorder(),
-                      ),
-                    ),
-                    Container(height: 16.0),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        isDense: true,
-                        labelText: 'Password',
-                        focusedBorder: _textFieldBorder(isFocused: true),
-                        enabledBorder: _textFieldBorder(),
-                      ),
-                    ),
-                    Container(height: 32.0),
-                    GenericButtonWidget('Login', () {
-                      print('login pressed');
-                      setState(() {
-                        _isLoading = true;
-                      });
+      body: _bodyDependingOnLoading(),
+    );
+  }
 
-                      Future.delayed(Duration(seconds: 1), () {
-                        setState(() {
-                          _isLoading = false;
-                        });
-                      });
-                    }),
-                    Container(height: 16.0),
-                    GenericButtonWidget('Register', () {
-                      print('register pressed');
-                      setState(() {
-                        _isLoading = true;
-                      });
+  Widget _bodyDependingOnLoading() {
+    if (_isLoading) {
+      return _progressIndicator();
+    } else {
+      return _contentLoginForm();
+    }
+  }
 
-                      Future.delayed(Duration(seconds: 1), () {
-                        setState(() {
-                          _isLoading = false;
-                        });
-                      });
-                    }),
+  Widget _progressIndicator() {
+    return Center(child: CircularProgressIndicator());
+  }
+
+  Widget _contentLoginForm() {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(height: 32.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.account_circle,
+                  color: Colors.blue.withOpacity(0.6),
+                  size: 64.0,
+                ),
+              ],
+            ),
+            Container(height: 16.0),
+            TextField(
+              decoration: InputDecoration(
+                isDense: true,
+                labelText: 'Username',
+                focusedBorder: _textFieldBorder(isFocused: true),
+                enabledBorder: _textFieldBorder(),
+              ),
+            ),
+            Container(height: 16.0),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                isDense: true,
+                labelText: 'Password',
+                focusedBorder: _textFieldBorder(isFocused: true),
+                enabledBorder: _textFieldBorder(),
+              ),
+            ),
+            Container(height: 32.0),
+            GenericButtonWidget('Login', () {
+              print('login pressed');
+              setState(() {
+                _isLoading = true;
+              });
+
+              Future.delayed(Duration(seconds: 1), () {
+                setState(() {
+                  _isLoading = false;
+                });
+              });
+            }),
+            Container(height: 16.0),
+            GenericButtonWidget('Register', () {
+              print('register pressed');
+              setState(() {
+                _isLoading = true;
+              });
+
+              Future.delayed(Duration(seconds: 1), () {
+                setState(() {
+                  _isLoading = false;
+                });
+              });
+            }),
 //            GestureDetector(
 //              onTapDown: (detail) {
 //                setState(() {
@@ -101,10 +116,9 @@ class _LoginPageState extends State<LoginPage> {
 //              },
 //              child: StaticLoginButtonWidget(_loginButtonColor),
 //            )
-                  ],
-                ),
-              ),
-            ),
+          ],
+        ),
+      ),
     );
   }
 
